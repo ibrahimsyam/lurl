6b6265e45a6c026dc3d3055df9e18b4756f66c6e
ibrahimsyam <ibrahimsyamgame@gmail.com>
2023-04-07 11:23:43 +0300

    Initial commit

---
+++ lurl.sh
@@ -0,0 +1,22 @@
+echo "enter url your for js file"
+read urljs
+
+echo "enter url your for js file"
+read url
+
+echo "python linkfinder.py -i $urljs -o cli | tee linkfind.txt"
+python linkfinder.py -i $urljs -o cli | tee linkfind.txt
+
+
+file_path="linkfind.txt"
+
+
+echo "------------------------"
+echo ""
+
+# read the list of words from the file and iterate over them
+while read -r name
+do
+  echo "$url$name"
+done < "$file_path"
+
